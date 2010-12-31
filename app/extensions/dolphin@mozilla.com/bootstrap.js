5f42610265cb629bd7492cde838456dbf15f652d
Irakli Gozalishvili <rfobic@gmail.com>
2010-12-31 16:18:30 +0100

    Creating a bootstrap extension to hook into startup.

---
+++ app/extensions/dolphin@mozilla.com/bootstrap.js
@@ -0,0 +1,16 @@
+
+function install(data, reason) {
+  dump('!!!!!!!!!')
+}
+function uninstall(data, reason) {
+  dump('###########')
+}
+
+function startup(data, reason) {
+  dump('>>>>>>>>')
+}
+function shutdown(data, reason) {
+  dump('<<<<<<<<<')
+}
+
+dump('~~~~~~~')
